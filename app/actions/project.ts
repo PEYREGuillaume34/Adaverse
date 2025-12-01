c8bb736e41866d29ebcb156d6f9db6c37c638a83
PeyreGuillaume34 <peyreguillaume34@gmail.com>
2025-12-01 15:24:24 +0100

    Push avant d'essayer le header dans Card Détails

--- app/actions/project.ts
+++ app/actions/project.ts
@@ -3,7 +3,7 @@
 import { revalidatePath } from "next/cache";
 import { db } from "../lib/db/drizzle";
 import { studentsTable, adaTable, promotionsTable } from "../lib/db/schema";
-import { eq } from "drizzle-orm";
+import { eq, desc } from "drizzle-orm";
 
 // ==================== HELPER ====================
 
